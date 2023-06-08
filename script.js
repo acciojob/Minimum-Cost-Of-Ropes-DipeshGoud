0adee58692e91d2ecbb17f9d453c20a067ea207f
DipeshGoud <112858278+DipeshGoud@users.noreply.github.com>
2023-06-08 12:35:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 function calculateMinCost() {
-  //your code here
-  
-  
-  
-}  
+  const input = document.getElementById('rope-lengths').value;
+  const lengths = input.split('').map(str => parseInt(str.trim()));
+
+  lengths.sort((a, b) => a - b);
+
+  let totalCost = 0;
+
+  while (lengths.length > 1) {
+    const smallest1 = lengths.shift();
+    const smallest2 = lengths.shift();
+
+    const cost = smallest1 + smallest2;
+    totalCost += cost;
+
+    lengths.unshift(cost); // Corrected line
+
+    lengths.sort((a, b) => a - b);
+  }
+
+  const resultElement = document.getElementById('result');
+  resultElement.textContent = totalCost.toString();
+}
